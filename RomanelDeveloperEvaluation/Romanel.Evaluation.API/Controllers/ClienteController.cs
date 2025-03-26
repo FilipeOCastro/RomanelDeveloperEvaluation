94046c45e2bcc0b82524de11b7e1dac9c0cedb0d
FilipeOCastro <filipe.oliveira.castro@gmail.com>
2025-03-26 10:43:47 -0300

    add solution

---
+++ RomanelDeveloperEvaluation/Romanel.Evaluation.API/Controllers/ClienteController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Romanel.Evaluation.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ClienteController : ControllerBase
+    {
+        private readonly ILogger<ClienteController> _logger;
+
+        public ClienteController(ILogger<ClienteController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetCliente")]
+        public IEnumerable<String> Get()
+        {
+            List<String> nomes = new List<String>();
+
+            nomes.Add("Teste1");
+            nomes.Add("Teste2");
+            nomes.Add("Teste3");
+
+            return nomes;
+        }
+    }
+}
+
